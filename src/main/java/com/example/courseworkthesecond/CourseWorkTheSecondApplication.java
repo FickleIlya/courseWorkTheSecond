1d05375ab4eb662eae1d58588502bceadd11e9b6
FickleIlya <fickle041@gmail.com>
2022-11-12 14:49:14 +0300

    init commit

---
+++ src/main/java/com/example/courseworkthesecond/CourseWorkTheSecondApplication.java
@@ -0,0 +1,13 @@
+package com.example.courseworkthesecond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWorkTheSecondApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseWorkTheSecondApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/courseworkthesecond/CourseWorkTheSecondApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.courseworkthesecond;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWorkTheSecondApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
